dfb02d6455cd7fd4b2a3ed7faea62cb587063e18
brassSyntax <leon.karpicz@gmail.com>
2017-12-08 11:41:31 +0200

    added readFile for File type parameter

--- src/RaceTextReader.java
+++ src/RaceTextReader.java
@@ -1,15 +1,33 @@
+import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
 
 public class RaceTextReader {
 
     public static String readFile(String filename) {
-        FileReader file;
+        FileReader fr;
         Scanner fileInput;
         StringBuilder text = new StringBuilder();
 
         try {
-            file = new FileReader(filename);
+            fr = new FileReader(filename);
+            fileInput = new Scanner(fr);
+
+            while (fileInput.hasNext()) {
+                text.append(fileInput.nextLine());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return text.toString();
+    }
+
+    public static String readFile(File file) {
+        Scanner fileInput;
+        StringBuilder text = new StringBuilder();
+
+        try {
             fileInput = new Scanner(file);
 
             while (fileInput.hasNext()) {
